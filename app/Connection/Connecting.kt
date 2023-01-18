0a6acd7462ce7f4650710804cae2ea5891741376
Antonio Mohorić <mohoricantonio@gmail.com>
2023-01-18 19:03:00 +0100

    Update
    Starting with the mobile app, changed UI a bit and tested it on the phone

---
+++ app/Connection/Connecting.kt
@@ -0,0 +1,5 @@
+import android.bluetooth.BluetoothAdapter
+
+public class Connecting {
+     
+}
--- app/src/main/java/com/example/remotecontrol/MainActivity.kt
+++ app/src/main/java/com/example/remotecontrol/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.remotecontrol
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnFwd: ImageButton = findViewById(R.id.btnForward);
+        btnFwd.setOnClickListener{
+            Toast.makeText(this, "F", Toast.LENGTH_SHORT).show()
+        }
+        val btnBck: ImageButton = findViewById(R.id.btnBack);
+        btnBck.setOnClickListener{
+            Toast.makeText(this, "B", Toast.LENGTH_SHORT).show()
+        }
+        val btnLft: ImageButton = findViewById(R.id.btnLeft);
+        btnLft.setOnClickListener{
+            Toast.makeText(this, "L", Toast.LENGTH_SHORT).show()
+        }
+        val btnRht: ImageButton = findViewById(R.id.btnRight);
+        btnRht.setOnClickListener{
+            Toast.makeText(this, "R", Toast.LENGTH_SHORT).show()
+        }
     }
 }
